d778112fe8dcea1974da755151b3cad51e6875e2
Kayky Fernandes <kaykyfernandes31@gmail.com>
2025-05-15 15:04:47 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+const friends = ['marcos', 'flavia', 'pedro', 'kayky']//arrays de string
+const num = [7, 8, 9]// array de number
+
+num.push(10, 11, 12)//envia no final
+
+num.unshift(1, 2, 3)// no começo
+
+num.splice(3, 0, 4, 5, 6)//regra para poder começar de um ponto de partida e apagar algum se precisar
+
+console.log(friends)//saida de dados
+console.log(num)//saida de dados 
